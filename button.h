6222ceab14160c7ae145e89e22aa9bc2482e1279
Riksev <102880184+Riksev@users.noreply.github.com>
2022-04-03 21:58:18 +0300

    Update button.h

--- button.h
+++ button.h
@@ -4,18 +4,24 @@
 #include "SFML/Graphics.hpp"
 class Button {
 private:
+	sf::Vector2f defaultSize;
 	sf::RectangleShape button;
 	sf::Text text;
+	sf::Sprite buttonSprite;
+	sf::Color textColor;
+	bool _hasSprite;
+	float goldProportion;
 
 public:
 	//Constructors
 	Button();
-	Button(std::string btnText, sf::Vector2f btnSize, sf::Vector2f position, int charSize, sf::Color bgColor, sf::Color textColor, sf::Font* font);
 
 	//Status
 	bool isMouseIn(sf::RenderWindow* target);
 
 	//Methods
+	void create(std::string btnText, sf::Vector2f btnSize, sf::Vector2f position, int charSize, sf::Color bgColor, sf::Color textColor, sf::Font* font);
+	void setSprite(sf::Texture* texture);
 	void setText(std::string info);
 	void setSize(sf::Vector2f size);
 	void setCharSize(unsigned int charSize);
@@ -23,6 +29,6 @@ public:
 	void setBackColor(sf::Color color);
 	void setTextColor(sf::Color color);
 	void setPosition(sf::Vector2f position);
-	void render(sf::RenderTarget* target);
+	void render(sf::RenderWindow* window);
 };
 #endif
